778750f415486c70c1dfb85ff355a17d2902119e
Jerry Belton <jerry.belton@gmail.com>
2017-03-04 16:33:15 -0800

    typo in quotes file

--- src/assets/js/quotes.js
+++ src/assets/js/quotes.js
@@ -2,8 +2,8 @@ $(function randomQquote(){
 	var myDict = {"If you must blink, do it now." : "Kubo",
 		"Yesterday is history, tomorrow is a mystery, and today is a gift... that's why it is called present." : "Master Oogway",
 		"I am incredibly nubby." : "Kyle",
-		"The past can hurt. But the way I see it, you can either run from it or learn from it" : "Rafiki"
-  "One day, life will flash before your eyes. Make sure it's worth watching.":""};
+		"The past can hurt. But the way I see it, you can either run from it or learn from it" : "Rafiki",
+    "One day, life will flash before your eyes. Make sure it's worth watching.":""};
 
 	var keys = Object.keys(myDict);
 
